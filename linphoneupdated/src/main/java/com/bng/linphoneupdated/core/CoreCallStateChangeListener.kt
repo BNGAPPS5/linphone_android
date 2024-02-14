a00be941e77667d2481b7bcfcaa3c3be2c5b609c
nisha <nisha@blackngreen.com>
2024-02-14 14:56:43 +0530

    Added linphone wrapper classes for Magiccall J4l

---
+++ linphoneupdated/src/main/java/com/bng/linphoneupdated/core/CoreCallStateChangeListener.kt
@@ -0,0 +1,20 @@
+package com.bng.linphoneupdated.core
+
+
+interface CoreCallStateChangeListener {
+    fun callIdle(message: String,protocolCode: Int)
+    fun callOutgoingInit(message: String)
+    fun callOutgoingRinging(message: String)
+    fun callOutgoingEarlyMedia(message: String)
+    fun callConnected(message: String)
+    fun callPausing(message: String)
+    fun callPaused(message: String)
+    fun callResuming(message: String)
+    fun callStreamsRunning(message: String)
+    fun callError(message: String,protocolCode:Int)
+    fun callReleased(message: String, protocolCode: Int)
+    fun callEnd(message: String,protocolCode: Int)
+    fun onMessageReceived(message: String)
+
+}
+
